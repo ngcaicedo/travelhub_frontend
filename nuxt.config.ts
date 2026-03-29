dc1e50d25201b81d7f15e4c89fbae60ebe487a9e
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2026-03-28 22:38:00 -0500

    feat: Enhance application configuration by adding a custom favicon and updating primary font to Plus Jakarta Sans. Update color scheme for improved branding and remove old favicon file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
