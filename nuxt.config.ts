042d5edcd12a092a7481fd164006b722a2fdd32b
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2026-03-28 22:06:24 -0500

    Update nuxt.config.ts to maintain lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
   ],
+  ssr: false,
 
   devtools: {
     enabled: true
